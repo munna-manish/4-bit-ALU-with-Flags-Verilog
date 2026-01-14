ab848a85c30a8f8082f34f8455cf50ecc4e4584b
Manish Gollapally <manishgollapally@gmail.com>
2026-01-14 11:22:52 +0530

    Add files via upload

---
+++ 4-bit-ALU(Design).v
@@ -0,0 +1,49 @@
+//DESIGN
+module alu(
+    input [3:0] a,
+    input [3:0] b,
+    input [1:0] sel,
+    output reg [3:0] y,
+    output reg [4:0] full_result,
+    output reg k,
+    output reg n,
+    output reg c
+);
+
+always @(*) begin
+    case(sel)
+        2'b00: begin
+            y=a+b;
+            full_result=a+b;
+        end
+        2'b01: begin
+            y=a-b;
+            full_result=a-b;
+        end
+        2'b10: begin
+            y=a&b; 
+            full_result=a&b;
+        end
+        2'b11: begin
+            y=a|b; 
+            full_result=a|b;
+        end
+    endcase
+
+    if (y==0)
+        k=1'b1;
+    else
+        k=1'b0;
+
+    if (y[3]==1)
+        n=1'b1;
+    else
+        n=1'b0;
+
+    if (full_result[4]==1)
+        c=1'b1;
+    else
+        c=1'b0;
+
+end
+endmodule
---
+++ 4-bit-ALU(Testbench).v
@@ -0,0 +1,20 @@
+//TESTBENCH
+module testbench;
+
+reg [3:0] a;
+reg [3:0] b;
+reg [1:0] sel;
+wire [3:0] y;
+wire [4:0] full_result;
+wire k,n,c;
+
+alu my_alu(a,b,sel,y,full_result,k,n,c);
+
+initial begin
+    a = 4'b1111;
+    b = 4'b0001;
+    sel = 2'b00;
+    #1;
+    $display("a=%b | b=%b | sel=%b | Y=%b, Carry=%b\nFlags: zero flag=%b | Negative flag=%b | Carry flag=%b\n", a, b, sel, y,c,k,n,c);
+end
+endmodule
