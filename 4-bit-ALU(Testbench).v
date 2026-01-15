6cfa741d87386c1c9826651607a03492c91d78c2
Manish Gollapally <manishgollapally@gmail.com>
2026-01-15 09:50:02 +0530

    Add overflow flag to ALU testbench

--- 4-bit-ALU(Testbench).v
+++ 4-bit-ALU(Testbench).v
@@ -6,15 +6,16 @@ reg [3:0] b;
 reg [1:0] sel;
 wire [3:0] y;
 wire [4:0] full_result;
-wire k,n,c;
+wire k,n,c,v;
 
-alu my_alu(a,b,sel,y,full_result,k,n,c);
+    alu my_alu(a,b,sel,y,full_result,k,n,c,v);
 
 initial begin
     a = 4'b1111;
     b = 4'b0001;
     sel = 2'b00;
     #1;
-    $display("a=%b | b=%b | sel=%b | Y=%b, Carry=%b\nFlags: zero flag=%b | Negative flag=%b | Carry flag=%b\n", a, b, sel, y,c,k,n,c);
+    $display("a=%b | b=%b | sel=%b | Y=%b, Carry=%b\nFlags: zero flag=%b | Negative flag=%b | Carry flag=%b | Overflow flag=%b\n", a, b, sel, y,c,k,n,c,v);
 end
 endmodule
+
